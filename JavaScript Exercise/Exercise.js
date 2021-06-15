406081925881afdd117ffbb51872372401a17149
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-06-15 23:56:03 +0530

    Program for sorting Strings in Ascending & Descending order

--- JavaScript Exercise/Exercise.js
+++ JavaScript Exercise/Exercise.js
@@ -6,11 +6,28 @@ function largestNumber()
     return(Math.max(...numbers));
 }
 
-console.log(`Largest number in an array: ' ${largestNumber}`)
+console.log(`Largest number in an array:  ${largestNumber()}`)
 
 
 
 //  Write a JavaScript program to display the current day and time in the following format mm/dd/yyyy.
 
 
+// Write a JavaScript program to sort array of strings in both ascending and descending order.
+let names = ["Heman","Goku","Ryu","Gohan","Kira"];
 
+function ascendingSort(){
+    names.sort();
+    console.log("Ascending Sort: ",names);
+}
+
+function descendingSort(){
+    names.sort((a,b)=> {
+       return b.localeCompare(a);
+    });
+    console.log("Descending Sort: ", names);
+}
+ascendingSort();
+descendingSort();
+
+//* Write a JavaScript program to remove duplicate items from an array.
