e346d76994b12f8054019a958dc6ca8ee63e86fd
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-06-15 14:00:31 +0530

    Javascript Exercises started....... Finding largest Number

---
+++ JavaScript Exercise/Exercise.js
@@ -0,0 +1,16 @@
+//* Write a JavaScript conditional statement to find the largest of five numbers.
+
+function largestNumber()
+{
+    let numbers=[452,7866,78945,12,82762];
+    return(Math.max(...numbers));
+}
+
+console.log(`Largest number in an array: ' ${largestNumber}`)
+
+
+
+//  Write a JavaScript program to display the current day and time in the following format mm/dd/yyyy.
+
+
+
