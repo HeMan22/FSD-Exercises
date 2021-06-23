9d80059b84a4e07b22fd324b0bc570bb2a84ed19
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-06-23 12:28:00 +0530

    Added Raw Data Extract Program

--- JavaScript Exercise/Exercise.js
+++ JavaScript Exercise/Exercise.js
@@ -199,3 +199,144 @@ console.log(
   }
   console.log(`'${letter}' in ${word} occured ${occurrence} times.`);
 })("himanshu", "h");
+
+/**
+ * * Consider the following object:
+var myObj =
+	[{'name':'Saurabh', 'age': 30, 'occupation': "Team Leader"},
+	{'name':'Anupriya', 'age': 32, 'occupation': "Team Leader"},
+	{'name':'Kalyani', 'age': 25, 'occupation': "Programmer"},
+	{'name':'Damodaran', 'age': 27, 'occupation': "Programmer"},
+	{'name':'Krishnakath', 'age': 22, 'occupation': "Programmer"},
+	{'name':'Venketraman', 'age': 28, 'occupation': "Programmer"}];
+
+
+ - Display all objects with occupation as 'Programmer'
+ - Sort the object based on age in descending order
+ - Create a new array containing only names present in myObj
+ - Recreate the above array of objects into the following object of objects :
+	{ 'Team Leader' : [{'name':_____________, 'age':__________},{}],
+	'<anotheroccupation>': [{'name':______________, 'age':________},{}]}
+ */
+console.log(`******************Object Refractor*************************`);
+function objectRefactor() {
+  var myObj = [
+    { name: "Saurabh", age: 30, occupation: "Team Leader" },
+    { name: "Anupriya", age: 32, occupation: "Team Leader" },
+    { name: "Kalyani", age: 25, occupation: "Programmer" },
+    { name: "Damodaran", age: 27, occupation: "Programmer" },
+    { name: "Krishnakath", age: 22, occupation: "Programmer" },
+    { name: "Venketraman", age: 28, occupation: "Programmer" },
+  ];
+  // - Display all objects with occupation as 'Programmer'
+  let programmerObjects = myObj.filter(
+    (obj) => obj.occupation === "Programmer"
+  );
+  console.log("All Programmers::", programmerObjects);
+  // - Sort the object based on age in descending order
+  let sortedByAge = myObj.sort((a, b) => {
+    return b.age - a.age;
+  });
+  console.log("Sorted By Age[DESC]::", sortedByAge);
+  // - Create a new array containing only names present in myObj
+  let namesArray = myObj.map((obj) => {
+    return obj.name;
+  });
+  console.log("Names Array::", namesArray);
+  /**
+      - Recreate the above array of objects into the following object of objects :
+    { 'Team Leader' : [{'name':_____________, 'age':__________},{}],
+    '<anotheroccupation>': [{'name':______________, 'age':________},{}]}
+    */
+  const modifiedObj = myObj.reduce(
+    (newObj, obj) => ({
+      ...newObj,
+      [obj.occupation]: [{ ...obj }],
+    }),
+    {}
+  );
+
+  console.log("Recreated Object::", modifiedObj);
+}
+
+objectRefactor();
+
+/**
+ * * Convert the following JSON string to an object and access the value of the 'chels' property.
+	var jsonStr = '{"mike":"Book","jason":"sweater","chels":"iPad"}';	
+ */
+console.log(`*********************JSON Conversion*************************`);
+function jsonConvertor() {
+  var jsonStr = '{"mike":"Book","jason":"sweater","chels":"iPad"}';
+  let obj = JSON.parse(jsonStr);
+  console.log("Object JSON::", obj);
+  console.log("chels:", obj.chels);
+}
+jsonConvertor();
+
+/**
+ * * Create a program that extracts data from the following JSON in the below format:
+[
+    {  author: .. , title: '' , description:'' },
+    {  author: '' , title: '' , description:'' },
+    {  author: .. , title: '' , description:'' },
+     .....
+]
+ */
+
+console.log(
+  `*************************** Raw Data Extract *********************************`
+);
+let rawData = {
+  status: "ok",
+  source: "the-next-web",
+  sortBy: "latest",
+  articles: [
+    {
+      author: "Bryan Clark",
+      title:
+        "Spotify is using billboards to call users out on their questionable listening habits",
+      description:
+        "In a hilarious new marketing campaign, Spotify is taking to billboards to pose some rather serious questions (and provide commentary) about its users. One of the new billboards, for example, will read: “Dear person who made a playlist called: ‘One Night Stand With Jeb Bush Like He’s a Bond Girl in a European Casino.’ We …",
+      url: "http://thenextweb.com/music/2016/11/30/spotify-is-using-billboards-to-call-users-out-on-their-questionable-listening-habits/",
+      urlToImage:
+        "https://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2016/11/Screen-Shot-2016-11-29-at-5.45.53-PM.png",
+      publishedAt: "2016-11-30T07:38:09Z",
+    },
+    {
+      author: "Bryan Clark",
+      title:
+        "Man in VR headset falls off fake cliff and hits a very real floor",
+      description:
+        "One day, we’ll have an entire Tumblr account for hilarious VR accidents, but until then, George Takei’s Facebook account will have to do. Takei, the Star Trek alum and current boss of sharing things on Facebook, recently shared a video of a man attempting to conquer a virtual cliff while inside an Oculus headset; it …",
+      url: "http://thenextweb.com/virtual-reality/2016/11/30/man-in-vr-headset-falls-off-fake-cliff-and-hits-a-very-real-floor/",
+      urlToImage:
+        "https://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2016/11/man-falling.png",
+      publishedAt: "2016-11-30T00:00:30Z",
+    },
+    {
+      author: "Napier Lopez",
+      title:
+        "Microsoft's new app tries to fix Android's copy and paste problem",
+      description:
+        "For an OS typically preferred by power-users, Android has an embarrassing amount of trouble with copy and paste. Sometimes it works just fine, but in other apps you can only copy entire huge swaths of text, or worse – nothing at all. Microsoft’s new Clip Layer app wants to fix that by letting you copy …",
+      url: "http://thenextweb.com/apps/2016/11/29/microsofts-new-app-tries-fix-androids-copy-paste-problem/",
+      urlToImage:
+        "https://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2016/11/Microsoft-Clip-Layer.png",
+      publishedAt: "2016-11-29T22:11:20Z",
+    },
+  ],
+};
+function extractData() {
+  let articles = rawData.articles;
+  //author: .. , title: '' , description:''
+  let newArticles = articles.map((article) => {
+    return {
+      author: article.author,
+      title: article.title,
+      description: article.description,
+    };
+  });
+  console.log(newArticles);
+}
+extractData();
